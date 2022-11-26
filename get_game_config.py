5dc6affc71f50dd18916899a3d2f8c05f76f3a36
Nerroth <nerroth@flappycurve.com>
2022-11-26 20:27:53 +0100

    Add missing config stuff from SE get_game_config.py

--- get_game_config.py
+++ get_game_config.py
@@ -2,16 +2,67 @@ import json
 import os
 import jsonpatch
 
-__game_config = json.load(open("./config/get_game_config.php_29august2012_nohash.txt", 'r'))
+__game_config = json.load(open("./config/main.json", 'r', encoding='utf-8'))
+
+def remove_duplicate_items():
+    indexes = {}
+    items = __game_config["items"]
+    num_duplicate = 0
+
+    while True:
+        index = 0
+        duplicate = False
+        for item in items:
+            if item["id"] in indexes:
+                del items[indexes[item["id"]]]
+                indexes.clear()
+                duplicate = True
+                num_duplicate += 1
+                break
+
+            indexes[item["id"]] = index
+            index += 1
+
+        if duplicate:
+            continue
+
+        print(f" * Removed {num_duplicate} duplicate items from config patches")
+        break
+
+def apply_config_patch(filename):
+    patch = json.load(open(filename, 'r'))
+    jsonpatch.apply_patch(__game_config, patch, in_place=True)
 
 def patch_game_config():
     patch_dir = "./config/patch"
+    mods_dir = "./mods"
+
     for patch_file in os.listdir(patch_dir):
-        if patch_file.endswith(".patch"):
+        if patch_file.endswith(".json"):
             f = os.path.join(patch_dir, patch_file)
-            patch = json.load(open(f, 'r'))
-            jsonpatch.apply_patch(__game_config, patch, in_place=True)
-            print(" * Patch applied:", patch_file)
+            apply_config_patch(f)
+            print(" * Patch applied:", f)
+
+    if os.path.exists("./mods.txt"):
+        with open("./mods.txt", "r") as f:
+            lines = f.readlines()
+            f.close()
+
+        for line in lines:
+            mod = line.strip()
+            if mod.startswith("#"):
+                continue
+            if mod != "":
+                mod.replace(".json", "")
+                mod_path = f"{mods_dir}/{mod}.json"
+                if os.path.exists(mod_path):
+                    apply_config_patch(mod_path)
+                    print(" * Mod applied:", mod)
+
+    remove_duplicate_items()
+
+print (" [+] Applying config patches...")
+patch_game_config()
 
 def get_game_config() -> dict:
     return __game_config
@@ -38,6 +89,8 @@ def get_level_from_xp(xp: int) -> int:
 # ITEMS #
 #########
 
+# ID
+
 items_dict_id_to_items_index = {int(item["id"]): i for i, item in enumerate(__game_config["items"])}
 
 def get_item_from_id(id: int) -> dict:
@@ -51,17 +104,24 @@ def get_attribute_from_item_id(id: int, attribute_name: str) -> str:
 def get_name_from_item_id(id: int) -> str:
     return get_attribute_from_item_id(id, "name")
 
+# subcat_functional
+
+items_dict_subcat_functional_to_items_index = {int(item["subcat_functional"]): i for i, item in enumerate(__game_config["items"])}
+
+def get_item_from_subcat_functional(subcat_functional: int) -> dict:
+    items_index = items_dict_subcat_functional_to_items_index[int(subcat_functional)] if int(subcat_functional) in items_dict_subcat_functional_to_items_index else None
+    return __game_config["items"][items_index] if items_index is not None else None
+
 ############
 # MISSIONS #
 ############
 
 # missions_dict_id_to_missions_index = {int(item["id"]): i for i, item in enumerate(__game_config["missions"])}
-# missions_dict_id_to_missions_index = {}
 
-# def get_mission_from_id(id: int) -> dict:
-#     items_index = missions_dict_id_to_missions_index[int(id)] if int(id) in missions_dict_id_to_missions_index else None
-#     return __game_config["missions"][items_index] if items_index is not None else None
-# 
-# def get_attribute_from_mission_id(id: int, attribute_name: str) -> str:
-#     mission = get_mission_from_id(id)
-#     return mission[attribute_name] if mission and attribute_name in mission else None
+def get_mission_from_id(id: int) -> dict:
+    items_index = missions_dict_id_to_missions_index[int(id)] if int(id) in missions_dict_id_to_missions_index else None
+    return __game_config["missions"][items_index] if items_index is not None else None
+
+def get_attribute_from_mission_id(id: int, attribute_name: str) -> str:
+    mission = get_mission_from_id(id)
+    return mission[attribute_name] if mission and attribute_name in mission else None
