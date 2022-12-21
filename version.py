29a19aeb79e05e57c5f43d7fbb3d29f2177404eb
Nerroth <nerroth@flappycurve.com>
2022-12-21 13:09:19 +0100

    Fixed inventory not working, apply inventory fix to saves if needed
    The inventory in the game was not loading because "inventoryItems" was null in player's privateState, so now the goal to view the Inventory in the game can be completed.

--- version.py
+++ version.py
@@ -3,11 +3,18 @@ from engine import timestamp_now
 version_name = "pre-alpha 0.01"
 version_code = "0.01a"
 
-def migrate_loaded_save(save: dict) -> bool:
+def migrate_loaded_save(save: dict):
     # 0.01a saves
+    _changed = False
     if "version" not in save:
+        _changed = True
         save["version"] = "0.01a"
-        
-        return True
+        print(" [!] Applied version to save")
 
-    return False
+    privateState = save["privateState"]
+    if privateState["inventoryItems"] == None:
+        _changed = True
+        privateState["inventoryItems"] = []
+        print(" [!] Applied inventory fix")
+
+    return _changed
